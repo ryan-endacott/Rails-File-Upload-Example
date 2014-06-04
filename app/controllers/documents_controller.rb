68bf6c61a84b06a7819e82b1ee5ae5e5ad9ffc5d
Ryan Endacott <rzeg24@gmail.com>
2014-06-03 23:27:40 -0400

    Cleanup and remove unneeded edit refs.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -27,7 +27,7 @@ class DocumentsController < ApplicationController
 
     respond_to do |format|
       if @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
+        format.html { redirect_to documents_path, notice: 'Document was successfully created.' }
         format.json { render action: 'show', status: :created, location: @document }
       else
         format.html { render action: 'new' }
