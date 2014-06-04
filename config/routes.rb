ea8a49970cdf19e26455f9674d1551ccf7bd6c64
Ryan Endacott <rzeg24@gmail.com>
2014-06-03 21:54:53 -0400

    Redirect route to /documents.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Fileupload::Application.routes.draw do
   resources :documents
 
+  root to: redirect('/documents')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
