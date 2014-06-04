15eeb15bac09603bc412b08f38fc320b3773b0ee
Ryan Endacott <rzeg24@gmail.com>
2014-06-03 22:32:22 -0400

    Save file in model initialization.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,19 @@
 class Document < ActiveRecord::Base
+
+  def initialize(params = {})
+    file = params.delete(:file)
+    super
+    if file
+      self.filename = sanitize_filename(file.original_filename)
+      self.content_type = file.content_type
+      self.file_contents = file.read
+    end
+  end
+
+  private
+    def sanitize_filename(filename)
+      # Get only the filename, not the whole path (for IE)
+      # Thanks to this article I just found for the tip: http://mattberther.com/2007/10/19/uploading-files-to-a-database-using-rails
+      return File.basename(filename)
+    end
 end
