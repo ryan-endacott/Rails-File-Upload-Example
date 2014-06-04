75107f710c1acd7831c3a8fd6971eadf0ae7438a
Ryan Endacott <rzeg24@gmail.com>
2014-06-03 23:18:45 -0400

    Validate file size under 1MB.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,19 +1,30 @@
 class Document < ActiveRecord::Base
 
+  validate :file_size_under_one_mb
+
   def initialize(params = {})
-    file = params.delete(:file)
+    @file = params.delete(:file)
     super
-    if file
-      self.filename = sanitize_filename(file.original_filename)
-      self.content_type = file.content_type
-      self.file_contents = file.read
+    if @file
+      self.filename = sanitize_filename(@file.original_filename)
+      self.content_type = @file.content_type
+      self.file_contents = @file.read
     end
   end
 
   private
+
     def sanitize_filename(filename)
       # Get only the filename, not the whole path (for IE)
       # Thanks to this article I just found for the tip: http://mattberther.com/2007/10/19/uploading-files-to-a-database-using-rails
       return File.basename(filename)
     end
+
+    NUM_BYTES_IN_MEGABYTE = 1048576
+    def file_size_under_one_mb
+      if (@file.size.to_f / NUM_BYTES_IN_MEGABYTE) > 1
+        errors.add(:file, 'File size cannot be over one megabyte.')
+      end
+    end
+
 end
